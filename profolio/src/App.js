60390b99ce3902fc71d6e08b9a2da310109381e9
gilbert <gilbert.bageni@student.moringaschool.com>
2023-11-23 00:58:28 +0300

    project setup inizilization for portifolio

--- profolio/src/App.js
+++ profolio/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import Banner from "./components/banner/Banner";
+import Banner from "./components/banner/Banner";
 import Contact from "./components/contact/Contact";
 import Features from "./components/features/Features";
 import Footer from "./components/footer/Footer";
 import FooterBottom from "./components/footer/FooterBottom";
 import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/projects/Projects";
-// import Resume from "./components/resume/Resume";
+import Resume from "./components/resume/Resume";
 // import Testimonial from "./components/tesimonial/Testimonial";
 
 function App() {
@@ -14,10 +14,10 @@ function App() {
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
         <Navbar />
       <div className="max-w-screen-xl mx-auto">
-        {/* <Banner /> */}
+        <Banner />
         <Features />
         <Projects />
-        {/* <Resume /> */}
+        <Resume />
         {/* <Testimonial /> */}
         <Contact />
         <Footer />
--- profolio/src/components/contact/Contact.js
+++ profolio/src/components/contact/Contact.js
@@ -1,57 +1,49 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import Title from '../layouts/Title';
 import ContactLeft from './ContactLeft';
 
 const Contact = () => {
-  const [username, setUsername] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const [successMsg, setSuccessMsg] = useState("");
+  const [username, setUsername] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
 
-  // ========== Email Validation start here ==============
   const emailValidation = () => {
-    return String(email)
-      .toLocaleLowerCase()
-      .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
+    return String(email).toLocaleLowerCase().match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
   };
-  // ========== Email Validation end here ================
 
   const handleSend = (e) => {
     e.preventDefault();
-    if (username === "") {
-      setErrMsg("Username is required!");
-    } else if (phoneNumber === "") {
-      setErrMsg("Phone number is required!");
-    } else if (email === "") {
-      setErrMsg("Please give your Email!");
+    if (username === '') {
+      setErrMsg('Username is required!');
+    } else if (phoneNumber === '') {
+      setErrMsg('Phone number is required!');
+    } else if (email === '') {
+      setErrMsg('Please give your Email!');
     } else if (!emailValidation(email)) {
-      setErrMsg("Give a valid Email!");
-    } else if (subject === "") {
-      setErrMsg("Plese give your Subject!");
-    } else if (message === "") {
-      setErrMsg("Message is required!");
+      setErrMsg('Give a valid Email!');
+    } else if (subject === '') {
+      setErrMsg('Please give your Subject!');
+    } else if (message === '') {
+      setErrMsg('Message is required!');
     } else {
-      setSuccessMsg(
-        `Thank you dear ${username}, Your Messages has been sent Successfully!`
-      );
-      setErrMsg("");
-      setUsername("");
-      setPhoneNumber("");
-      setEmail("");
-      setSubject("");
-      setMessage("");
+      setSuccessMsg(`Thank you dear ${username}, Your Messages has been sent Successfully!`);
+      setErrMsg('');
+      setUsername('');
+      setPhoneNumber('');
+      setEmail('');
+      setSubject('');
+      setMessage('');
     }
   };
+
   return (
-    <section
-      id="contact"
-      className="w-full py-20 border-b-[1px] border-b-black"
-    >
+    <section id="contact" className="w-full py-20 border-b-[1px] border-b-black">
       <div className="flex justify-center items-center text-center">
-        <Title title="CONTACT" des="Contact With Me" />
+        <Title title="Contact" des="Contact With Me" />
       </div>
       <div className="w-full">
         <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
@@ -70,72 +62,59 @@ const Contact = () => {
               )}
               <div className="w-full flex flex-col lgl:flex-row gap-10">
                 <div className="w-full lgl:w-1/2 flex flex-col gap-4">
-                  <p className="text-sm text-gray-400 uppercase tracking-wide">
-                    Your name
-                  </p>
+                  <p className="text-sm text-gray-400 uppercase tracking-wide">Your name</p>
                   <input
+                    id="username"
                     onChange={(e) => setUsername(e.target.value)}
                     value={username}
                     className={`${
-                      errMsg === "Username is required!" &&
-                      "outline-designColor"
-                    } contactInput`}
+                      errMsg === 'Username is required!' && 'outline-designColor'
+                    } contactInput bg-gray-800 text-white px-3 py-2 rounded-md focus:outline-none focus:ring focus:border-designColor`}
                     type="text"
                   />
                 </div>
                 <div className="w-full lgl:w-1/2 flex flex-col gap-4">
-                  <p className="text-sm text-gray-400 uppercase tracking-wide">
-                    Phone Number
-                  </p>
+                  <p className="text-sm text-gray-400 uppercase tracking-wide">Phone Number</p>
                   <input
                     onChange={(e) => setPhoneNumber(e.target.value)}
                     value={phoneNumber}
                     className={`${
-                      errMsg === "Phone number is required!" &&
-                      "outline-designColor"
-                    } contactInput`}
+                      errMsg === 'Phone number is required!' && 'outline-designColor'
+                    } contactInput bg-gray-800 text-white px-3 py-2 rounded-md focus:outline-none focus:ring focus:border-designColor`}
                     type="text"
                   />
                 </div>
               </div>
               <div className="flex flex-col gap-4">
-                <p className="text-sm text-gray-400 uppercase tracking-wide">
-                  Email
-                </p>
+                <p className="text-sm text-gray-400 uppercase tracking-wide">Email</p>
                 <input
                   onChange={(e) => setEmail(e.target.value)}
                   value={email}
                   className={`${
-                    errMsg === "Please give your Email!" &&
-                    "outline-designColor"
-                  } contactInput`}
+                    errMsg === 'Please give your Email!' && 'outline-designColor'
+                  } contactInput bg-gray-800 text-white px-3 py-2 rounded-md focus:outline-none focus:ring focus:border-designColor`}
                   type="email"
                 />
               </div>
               <div className="flex flex-col gap-4">
-                <p className="text-sm text-gray-400 uppercase tracking-wide">
-                  Subject
-                </p>
+                <p className="text-sm text-gray-400 uppercase tracking-wide">Subject</p>
                 <input
                   onChange={(e) => setSubject(e.target.value)}
                   value={subject}
                   className={`${
-                    errMsg === "Plese give your Subject!" &&
-                    "outline-designColor"
-                  } contactInput`}
+                    errMsg === 'Please give your Subject!' && 'outline-designColor'
+                  } contactInput bg-gray-800 text-white px-3 py-2 rounded-md focus:outline-none focus:ring focus:border-designColor`}
                   type="text"
                 />
               </div>
               <div className="flex flex-col gap-4">
-                <p className="text-sm text-gray-400 uppercase tracking-wide">
-                  Message
-                </p>
+                <p className="text-sm text-gray-400 uppercase tracking-wide">Message</p>
                 <textarea
                   onChange={(e) => setMessage(e.target.value)}
                   value={message}
                   className={`${
-                    errMsg === "Message is required!" && "outline-designColor"
-                  } contactTextArea`}
+                    errMsg === 'Message is required!' && 'outline-designColor'
+                  } contactTextArea bg-gray-800 text-white px-3 py-2 rounded-md focus:outline-none focus:ring focus:border-designColor`}
                   cols="30"
                   rows="8"
                 ></textarea>
@@ -164,6 +143,6 @@ const Contact = () => {
       </div>
     </section>
   );
-}
+};
 
-export default Contact
+export default Contact;
