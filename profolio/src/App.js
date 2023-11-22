db1a785e70929f4387510479b5ef173230fac5bb
gilbert <gilbert.bageni@student.moringaschool.com>
2023-11-22 16:55:15 +0300

    added tailwind and app.js and index content

--- profolio/src/App.js
+++ profolio/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-5xl font-bold underline text-yellow-500">
+      <h1 className="text-5xl font-bold underline text-green-500">
       Hello world!
     </h1>
       </div>
