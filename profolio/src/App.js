c68989ed2cc5bd4cf915ee27bb1318592610e107
gilbert <gilbert.bageni@student.moringaschool.com>
2023-11-22 13:16:26 +0300

    committed

---
+++ profolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-5xl font-bold underline text-yellow-500">
+      Hello world!
+    </h1>
+      </div>
+  );
+}
+
+export default App;
