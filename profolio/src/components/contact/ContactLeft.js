9c9f5faadd8d20e7dbd87c4570279f9aece59651
gilbert <gilbert.bageni@student.moringaschool.com>
2023-11-24 01:19:33 +0300

    finalizing changes

--- profolio/src/components/contact/ContactLeft.js
+++ profolio/src/components/contact/ContactLeft.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import { contactImg } from "../../assets/index";
+import React from 'react';
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
+import { contactImg } from '../../assets/index';
 
 const ContactLeft = () => {
   return (
@@ -12,22 +12,19 @@ const ContactLeft = () => {
       />
       <div className="flex flex-col gap-4">
         <h3 className="text-3xl font-bold text-white">BAGENI GILBERT</h3>
-        <p className="text-lg font-normal text-gray-400">
-          Full Stack Developer
-        </p>
+        <p className="text-lg font-normal text-gray-400">Full Stack Developer</p>
         <p className="text-base text-gray-400 tracking-wide">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
-          ipsam autem cumque, accusantium dicta odio.
+          I am a passionate Full Stack Developer dedicated to creating innovative solutions.
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Phone: <span className="text-lightText">+254 746741719</span>
+          Phone: <span className="text-lightText">+254 746 741 719</span>
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
           Email: <span className="text-lightText">bagenigilbert@gmail.com</span>
         </p>
       </div>
       <div className="flex flex-col gap-4">
-        <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
+        <h2 className="text-base uppercase font-titleFont mb-4">Find me on</h2>
         <div className="flex gap-4">
           <span className="bannerIcon">
             <FaFacebookF />
@@ -42,6 +39,6 @@ const ContactLeft = () => {
       </div>
     </div>
   );
-}
+};
 
-export default ContactLeft
+export default ContactLeft;
