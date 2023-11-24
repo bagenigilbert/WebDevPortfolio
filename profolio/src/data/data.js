48391e68423b15ce2fab7f84209b2f3584df0576
gilbert <gilbert.bageni@student.moringaschool.com>
2023-11-24 18:50:26 +0300

    finalizing changes

--- profolio/src/data/data.js
+++ profolio/src/data/data.js
@@ -14,30 +14,32 @@ export const featuresData = [
     id: 2,
     icon: <AiFillAppstore />,
     title: "App Development",
-    des: "With a strong foundation in JavaScript and React, I specialize in developing powerful and user-friendly applications that meet the unique needs of your business.",
+    des: "With a strong foundation in JavaScript and React, I specialize in developing powerful and user-friendly applications.",
   },
   {
     id: 3,
     icon: <SiProgress />,
-    title: "SEO Optimization",
-    des: "Utilizing my skills in Python, Flask, and Django, I implement SEO optimization strategies to enhance your online visibility and drive organic traffic to your digital platforms.",
+    title: "Cross-functional Collaboration",
+    des: "Collaborating with cross-functional teams, including designers, product managers, and other developers.",
   },
   {
     id: 4,
     icon: <FaMobile />,
-    title: "Mobile Development",
-    des: "I excel in mobile development, creating responsive and efficient applications that provide a seamless user experience, powered by React and other cutting-edge technologies.",
+    title: "Social Media Integration",
+    des: "Enhance user engagement with seamless social media features for sharing and interaction.",
   },
+  
+  
   {
     id: 5,
     icon: <SiAntdesign />,
     title: "UX Design",
-    des: "My proficiency in UX design ensures that every digital solution I create is not only functional but also aesthetically pleasing, delivering an exceptional user experience.",
+    des: "My proficiency in UX design ensures that every digital solution I create is not only functional but also aesthetically pleasing.",
   },
   {
     id: 6,
     icon: <FaGlobe />,
     title: "Hosting Websites",
-    des: "With expertise in HTML, CSS, Tailwind CSS, and more, I provide reliable hosting solutions for websites, ensuring optimal performance and a secure online presence.",
+    des: "Transforming your online presence with expert hosting solutions.I guarantee optimal performance and a secure environment for your websites.",
   },
 ];
